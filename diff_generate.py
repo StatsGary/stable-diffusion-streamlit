09c7db5a27716099a9db19d3d3916d116e800b81
Gary Hutson <gary.hutson.uk@hotmail.co.uk>
2022-12-19 15:43:26 +0000

    Added prompt code to get it working

--- diff_generate.py
+++ diff_generate.py
@@ -3,45 +3,35 @@ from diffusers import StableDiffusionPipeline
 from torch import autocast
 
 class StableDiffusionLoader:
-    def __init__(self, prompt, pretrain_pipe='CompVis/stable-diffusion-v1-4'):
+    def __init__(self, 
+                prompt:str, 
+                pretrain_pipe:str='CompVis/stable-diffusion-v1-4'):
         self.prompt = prompt
         self.pretrain_pipe = pretrain_pipe
+        assert isinstance(self.prompt, str), 'Please enter a string into the prompt field'
+        assert isinstance(self.pretrain_pipe, str), 'Please use value such as `CompVis/stable-diffusion-v1-4` for pretrained pipeline'
 
 
-    def create_pipe(self):
-        self.pipe = StableDiffusionPipeline.from_pretrained(self.pretrain_pipe, 
+    def generate_image_from_prompt(self, save_location='prompt.jpg', use_token=False,
+                                   verbose=False):
+        pipe = StableDiffusionPipeline.from_pretrained(self.pretrain_pipe, 
             revision="fp16", torch_dtype=torch.float16, 
-            use_auth_token=False)
+            use_auth_token=use_token)
 
         pipe = pipe.to('cuda')
-        return pipe
-
-    def generate_image_from_prompt(self):
         with autocast('cuda'):
-            image = self.pipe(self.prompt)[0][0]
-        image.save(f'prompt.jpg')
+            image = pipe(self.prompt)[0][0]
+        if verbose: 
+            print('[INFO] saving image to desired location')
+
+        image.save(save_location)
         return image    
 
+    def __str__(self) -> str:
+        return f'Generating image for prompt {self.prompt}'
 
-st = StableDiffusionLoader('man on crutches')
-st.create_pipe()
-st.generate_image_from_prompt()
-
-
-
-# PROMPT = 'a man on a scooter'
-
-# pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", 
-#     revision="fp16", torch_dtype=torch.float16, 
-#     use_auth_token=False)
-
-# pipe = pipe.to('cuda')
-
-# prompt = PROMPT
-# with autocast('cuda'):
-#     image = pipe(prompt)[0][0]
-
-# image.save(f'prompt.jpg')
-
-
+if __name__ == '__main__':
+    # Intantiate class with relevant prompt
+    st = StableDiffusionLoader('homer simpson on the computer keyboard wearing a space suit')
+    st.generate_image_from_prompt(verbose=True)
 
