dea7f65741b8f7912409383c7b6c7b73243ecfa6
Gary Hutson <gary.hutson.uk@hotmail.co.uk>
2022-12-20 12:33:01 +0000

    Added style sheets

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,10 +76,13 @@ class StableDiffusionLoader:
         return len(self.prompt)
 
 if __name__ == '__main__':
+   
     SAVE_LOCATION = 'prompt.jpg'
     # Create the page title 
     st.set_page_config(page_title='Diffusion Model generator', page_icon='figs/favicon.ico')
     # Create page layout
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     st.title('Image generator using Stable Diffusion')
     st.caption('An app to generate images based on text prompts with a :blue[_Stable Diffusion_] model :sunglasses:')
     # Create a sidebar with text examples
